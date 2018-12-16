c2beab8f6a28fef77c5cf7642b857268508c0230
meirelon <nestelm@gmail.com>
2018-12-15 20:58:40 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+import argparse
+import bot
+
+
+def main(argv=None):
+	parser = argparse.ArgumentParser()
+
+	parser.add_argument('--chromedriver_path',
+	                    dest='chromedriver_path',
+	                    default = None,
+	                    help='Chromedriver Path')
+	parser.add_argument('--username',
+	                    dest='username',
+	                    default = None,
+	                    help='Username')
+	parser.add_argument('--password',
+	                    dest='password',
+	                    default = None,
+	                    help='Password')
+	parser.add_argument('--file_path',
+	                    dest='file_path',
+	                    default = None,
+	                    help='File Path')
+	parser.add_argument('--pages',
+	                    dest='pages',
+	                    default = int(200),
+	                    help='This is the GCP project you wish to send the data')
+	parser.add_argument('--hashtag_list',
+	                    dest='hashtag_list',
+	                    default = "travelblog travelblogger traveler",
+	                    help='This is the GCP project you wish to send the data')
+
+	args, _ = parser.parse_known_args(argv)
+
+	instagram_bot = bot.InstagramBot(chromedriver_path=args.chromedriver_path,
+									username=args.username,
+									password=args.password)
+	instagram_bot.login()
+	instagram_bot.follow_hashtags(file_path=args.file_path)
+
+if __name__ == '__main__':
+	main()
