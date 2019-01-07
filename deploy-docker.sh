a3c06def016740e1a99db2bd631ad4cff3ace3aa
meirelon <nestelm@gmail.com>
2019-01-06 21:41:17 -0500

    deploy docker image

---
+++ deploy-docker.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Print sub commands and fail on error
+set -ex
+
+REPOSITORY=
+IMAGE_LOCATION=gcr.io/scarlet-labs
+RUN_IMAGE_NAME=selenium-py37-image
+RUN_IMAGE_VERSION=$(date +"%Y%m%dT%H%M%S")-$(git rev-parse --short HEAD)
+
+mkdir -p target/docker
+
+# Write tags to files similar to how docker-maven-plugin does it
+echo $IMAGE_LOCATION/$RUN_IMAGE_NAME:$RUN_IMAGE_VERSION > target/docker/image-name
+
+# Build docker image, name and tag it, and push to registry.
+gcloud docker -- build -f selenium.docker -t $IMAGE_LOCATION/$RUN_IMAGE_NAME:$RUN_IMAGE_VERSION -t $IMAGE_LOCATION/$RUN_IMAGE_NAME:latest .
+gcloud docker -- push $IMAGE_LOCATION/$RUN_IMAGE_NAME:$RUN_IMAGE_VERSION
+gcloud docker -- push $IMAGE_LOCATION/$RUN_IMAGE_NAME:latest
+
+echo "Successfully built image as version $RUN_IMAGE_VERSION"
