6e3d581f7a0711bba2074fd86a00866acea19729
meirelon <nestelm@gmail.com>
2019-01-11 22:28:04 -0500

    updated deploy docker

--- deploy-docker.sh
+++ deploy-docker.sh
@@ -5,7 +5,7 @@ set -ex
 
 REPOSITORY=
 IMAGE_LOCATION=gcr.io/scarlet-labs
-RUN_IMAGE_NAME=selenium-py37-image
+RUN_IMAGE_NAME=selenium-py36-image
 RUN_IMAGE_VERSION=$(date +"%Y%m%dT%H%M%S")
 
 mkdir -p target/docker
