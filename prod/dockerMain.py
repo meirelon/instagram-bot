c06bfa7f13d4ca847a50b132997cb63d1e2c3407
meirelon <nestelm@gmail.com>
2019-01-13 11:55:56 -0500

    insert latest run into master table

--- prod/dockerMain.py
+++ prod/dockerMain.py
@@ -2,8 +2,23 @@ import os
 import logging
 import argparse
 import re
+from time import sleep
 import dockerBot
 import pandas as pd
+
+from gcloud import bigquery
+
+def async_query(query, project_id, dataset_id, dest_table):
+    client = bigquery.Client.from_service_account_json("scarlet-labs.json")
+    query_job = client.run_async_query(str(uuid.uuid4()), query)
+    query_job.use_legacy_sql = False
+    dataset = bigquery.Dataset(dataset_id, client)
+    table = bigquery.Table(dest_table, dataset)
+    query_job.destination = table
+    query_job.create_disposition = "CREATE_IF_NEEDED"
+    query_job.write_disposition = "WRITE_TRUNCATE"
+    query_job.begin()
+    return True
 
 
 def main(argv=None):
@@ -40,6 +55,12 @@ def main(argv=None):
 	login_webdriver = instagram_bot.login()
 
 
+	final_table_query = """select *
+							from `scarlet-labs.instagram.latest_run`
+							union all(
+							select * from `scarlet-labs.instagram.followed_master_table`)""".format(destination_table=args.destination_table)
+
+
 	if bool(re.search(string=args.hashtag_list.lower(), pattern="[.]csv")):
 		hashtag_list = [x[1:].strip()
             for x in pd.read_csv(args.hashtag_list, header=None)[0].values if len(x) > 0]
@@ -52,10 +73,16 @@ def main(argv=None):
 			df = instagram_bot.follow_hashtag(webdriver=login_webdriver, hashtag=hashtag, pages=args.pages)
 			df.to_gbq(project_id=args.project_id,
 										 private_key="scarlet-labs.json",
-										 destination_table="instagram.{destination_table}".format(destination_table=args.destination_table),
-										 if_exists="append",
+										 destination_table="instagram.latest_run",
+										 if_exists="replace",
 										 chunksize=100,
 										 verbose=True)
+
+			sleep(2)
+			async_query(query=final_table_query,
+						project_id=args.project_id,
+						dataset_id="instagram",
+						dest_table=args.destination_table)
 		except Exception as e:
 			print(e)
 			logging.debug(e)
