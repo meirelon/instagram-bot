bbca7a27cecaff6b39ce3eb2b6c13028acd190f4
meirelon <nestelm@gmail.com>
2019-01-29 10:40:04 -0500

    updated docker deploy

--- deploy-docker.sh
+++ deploy-docker.sh
@@ -3,9 +3,9 @@
 # Print sub commands and fail on error
 set -ex
 
-REPOSITORY=
+FILE_NAME=dockercompose.docker
 IMAGE_LOCATION=gcr.io/scarlet-labs
-RUN_IMAGE_NAME=selenium-py36-image
+RUN_IMAGE_NAME=selenium-py36-image-new
 RUN_IMAGE_VERSION=$(date +"%Y%m%dT%H%M%S")
 
 mkdir -p target/docker
@@ -14,7 +14,7 @@ mkdir -p target/docker
 echo $IMAGE_LOCATION/$RUN_IMAGE_NAME:$RUN_IMAGE_VERSION > target/docker/image-name
 
 # Build docker image, name and tag it, and push to registry.
-gcloud docker -- build -f selenium.docker -t $IMAGE_LOCATION/$RUN_IMAGE_NAME:$RUN_IMAGE_VERSION -t $IMAGE_LOCATION/$RUN_IMAGE_NAME:latest .
+gcloud docker -- build -f $FILE_NAME -t $IMAGE_LOCATION/$RUN_IMAGE_NAME:$RUN_IMAGE_VERSION -t $IMAGE_LOCATION/$RUN_IMAGE_NAME:latest .
 gcloud docker -- push $IMAGE_LOCATION/$RUN_IMAGE_NAME:$RUN_IMAGE_VERSION
 gcloud docker -- push $IMAGE_LOCATION/$RUN_IMAGE_NAME:latest
 
